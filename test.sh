1cd1a40d9e534187d9424be7cb753caed68e150c
Steven Herbst <sherbst@stanford.edu>
2022-02-21 12:49:41 -0800

    bug fix

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@
 # exit if a command fails
 set -e
 
+# go to the directory where this repository is located
+# see .github/workflows/regress.yml for mount definition
+cd /tmp
+
 # make directory for build output
 mkdir -p lib
 
